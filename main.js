038741f6ccf6515c4cd2780d22007b1f8f77829b
Kelsey Brisson <klbrisson@gmail.com>
2014-04-25 18:02:34 -0600

    Fixed bug of unique id not matching quote list object key. Now, quote id's are re-generated each time by the quoteList constructor. If quote is not within QuoteList object, id will be undefined unless set elsewhere. Star ratings work display but do not save yet. TO DO: save star ratings in quote object to be saved in local storage; Create sort function to sort by ratings.

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 
-// Prepopulate some quotes
-var existingQuotes =
-[["Life isn’t about getting and having, it’s about giving and being.", "Kevin Kruse"],
-["Whatever the mind of man can conceive and believe, it can achieve.", "Napoleon Hill"],
-["Strive not to be a success, but rather to be of value.", "Albert Einstein"],
-["Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.", "Robert Frost"],
-["I attribute my success to this: I never gave or took any excuse.", "Florence Nightingale"],
-["You miss 100% of the shots you don’t take.", "Wayne Gretzky"],
-["I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games." +
-" 26 times I’ve been trusted to take the game winning shot and missed." +
-" I’ve failed over and over and over again in my life. And that is why I succeed.", "Michael Jordan"],
-["The most difficult thing is the decision to act, the rest is merely tenacity.", "Amelia Earhart"],
-["Every strike brings me closer to the next home run.", "Babe Ruth"]];
+// // Prepopulate some quotes
+// var existingQuotes =
+// [["Life isn’t about getting and having, it’s about giving and being.", "Kevin Kruse"],
+// ["Whatever the mind of man can conceive and believe, it can achieve.", "Napoleon Hill"],
+// ["Strive not to be a success, but rather to be of value.", "Albert Einstein"],
+// ["Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.", "Robert Frost"],
+// ["I attribute my success to this: I never gave or took any excuse.", "Florence Nightingale"],
+// ["You miss 100% of the shots you don’t take.", "Wayne Gretzky"],
+// ["I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games." +
+// " 26 times I’ve been trusted to take the game winning shot and missed." +
+// " I’ve failed over and over and over again in my life. And that is why I succeed.", "Michael Jordan"],
+// ["The most difficult thing is the decision to act, the rest is merely tenacity.", "Amelia Earhart"],
+// ["Every strike brings me closer to the next home run.", "Babe Ruth"]];
 
 
 
@@ -22,29 +22,46 @@ function isInputValid (input) {
 
 // QUOTE CREATION
 // Creates unique ID
-var uniqueID = 0;
-function unique() {
-	return uniqueID++;
-}
+// localStorage.setItem('uniqueID', uniqueID);
+
+// function unique() {
+// 	var uniqueID = localStorage.getItem('uniqueID');
+// 	+uniqueID++;
+// 	localStorage.setItem('uniqueID', uniqueID);
+// 	console.log(uniqueID);
+// 	return uniqueID;
+
+// }
+
 // Quote Constructor
-function Quote (text, author) {
+function Quote (text, author, id) {
 	this.text = text;
 	this.author = author;
-	this.id = unique();
+	this.rating = 0;
+	this.id = id;
 }
+
+
+
+
 
 // QuoteList Object constructor
 function QuoteList (obj) {
+	var j = 0;
 	if (arguments.length === 0) {
 	}
 	else if (arguments.length === 1 && obj.id === undefined) {
 		for (key in obj) {
-			this[key] = obj[key];
+			var quote = new Quote(obj[key].text, obj[key].author, j);
+			this[j] = quote;
+			console.log(quote);
+			j++;
 		}
 	}
 	else if (typeof obj.id !== undefined) {
 		for (var i=0; i<arguments.length; i++){
-			this[arguments[i].id] = arguments[i];
+			this[j] = arguments[i];
+			j++;
 		}
 	}
 }
@@ -70,7 +87,12 @@ function renderQuotes(objOfQuotes) {
 
 // adds a quote object to a given quoteList
 function addQuote (quoteObj, quotesList) {
-	quotesList[quoteObj.id] = quoteObj;
+	var length = 0;
+	for (key in quotesList) {
+		length++;
+	}
+	quoteObj.id = length;
+	quotesList[length] = quoteObj;
 }
 
 // filter quotes by author
@@ -84,43 +106,35 @@ function filterByAuthor(quotesList, author) {
 	return authorQuotes;
 }
 
+function setRating(rating) {
+	for (i=0; i <= 5; i++) {
+		var star = $('[data-rating="' + rating + '"]').find('[data-star="' + i + '"]');
+		i <= +rating ? star.text('★') : star.text('☆');
+	}
+}
+
+function saveAndRender(item) {
+	localStorage.setItem('quotes',JSON.stringify(item));
+	renderQuotes(item);
+}
+
 
 
 
 // Get quotes from local storage, re-create quote objects, render quotes
+// if (localStorage.getItem('quotes') !== null){
 var allQuotes = new QuoteList(JSON.parse(localStorage.getItem('quotes')));
-for (key in allQuotes) {
-	allQuotes[key] = new Quote(allQuotes[key].text, allQuotes[key].author);
-}
-renderQuotes(allQuotes);
-
-
-
-
-
-
-
-// var quoteArray = JSON.parse(localStorage.getItem('quotes'));
-// quoteArray = quoteArray.map(function(obj) {
-// 	return new Quote(obj.text, obj.author);
-// });
-// console.log(quoteArray);
-
-
-
-// Initial quotes - takes an array of quote/author strings, creates objects, then maps through
-// the array to add the quote objects to an object for all quotes by unique id
-// var quoteArray = [];
-
-
-// for (var i=0; i<existingQuotes.length; i++) {
-// 	 quoteArray.push( new Quote (existingQuotes[i][0], existingQuotes[i][1]) );
 // }
-// var allQuotes = new QuoteList();
-// quoteArray.map(function(obj) {
-// 	addQuote(obj, allQuotes);
-// });
-// renderQuotes(allQuotes);
+// else {
+// 	var allQuotes = new QuoteList();
+// }
+// for (key in allQuotes) {
+// 	allQuotes[key] = new Quote(allQuotes[key].text, allQuotes[key].author);
+// }
+renderQuotes(allQuotes); 
+
+
+
 
 
 
@@ -144,8 +158,7 @@ $(document).on('ready', function() {
 		if (isInputValid(quoteText) && isInputValid(quoteAuthor)) {
 			var newQuote = new Quote(quoteText,quoteAuthor);
 			addQuote(newQuote,allQuotes);
-			renderQuotes(allQuotes);
-			localStorage.setItem('quotes',JSON.stringify(allQuotes));
+			saveAndRender(allQuotes);
 			$(this)[0].reset();
 			$(this).toggle();
 		}
@@ -163,10 +176,22 @@ $(document).on('ready', function() {
 		var quoteID = $(this).closest('.quote').attr('data-id');
 		console.log(quoteID);
 		delete allQuotes[quoteID];
-		localStorage.setItem('quotes',JSON.stringify(allQuotes));
-		renderQuotes(allQuotes);
+		saveAndRender(allQuotes);
 	});
+
+
+	// Star Ratings
+	$(document).on('click','.star', function() {
+		var rating = $(this).attr('data-star');
+		$(this).closest('.rating').attr('data-rating', rating);
+		setRating(rating);
+	});
+
+
+
 });
+
+
 
 
 
